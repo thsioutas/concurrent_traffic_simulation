30535caf0bdbae4af502361ffb56342b4f56a94c
Thomas Sioutas <thomas.sioutas@prodrive-technologies.com>
2020-06-16 14:53:52 +0200

    Implement TrafficLight::cycleThroughPhases and TrafficLight::simulate

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -23,7 +23,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -31,8 +31,8 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
+    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
+    // runs and repeatedly calls the receive function on the message queue.
     // Once it receives TrafficLightPhase::green, the method returns.
 }
 
@@ -43,7 +43,9 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the
+    // public method „simulate“ is called. To do this, use the thread queue in the base class.
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +54,31 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+    auto timeLastUpdate = std::chrono::system_clock::now();
+
+    // Set a new cycle duration value between 4 and 6 seconds
+    auto cycleDuration = std::chrono::seconds(rand() % 3 + 4);
+    while(true) {
+        // Sleep to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        // Compute time since last update
+        auto timeSinceLastUpdate = std::chrono::system_clock::now() - timeLastUpdate;
+        if (timeSinceLastUpdate >= cycleDuration) {
+            // Toggle _currentPhase
+            if (_currentPhase == TrafficLightPhase::red) {
+                _currentPhase = TrafficLightPhase::green;
+            } else {
+                _currentPhase = TrafficLightPhase::red;
+            }
+
+            // TODO(thosio): Send update to message queue using move semantics
+
+            // Update timeLastUpdate
+            timeLastUpdate = std::chrono::system_clock::now();
+        }
+    }
 }
 
-*/
